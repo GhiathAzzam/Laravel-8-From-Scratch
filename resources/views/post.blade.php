c6282f1ac5aed0a1257b2c48a9bba60621de8b7d
GhiathAzzam <ghiathazzam303@gmail.com>
2023-10-20 18:12:32 -0500

    First Commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<?= $post; ?>
+
+<a href="/">GO Back Home</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+    <p>
+        Qusai is a Bitch
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+    <p>
+        Qusai is a Bitch
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+    <p>
+        Qusai is a Bitch
+    </p>
+</article>
+</body>
